576d652d6f3fe005739fbf9300a5f648a13d1c83
KathanrDave <108331571+KathanrDave@users.noreply.github.com>
2023-01-29 00:21:51 +0530

    modified drawer

--- listngo/lib/widgets/user_drawer.dart
+++ listngo/lib/widgets/user_drawer.dart
@@ -13,89 +13,100 @@ class _drawerWidgetState extends State<drawerWidget> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.only(
-            topRight: Radius.circular(20), bottomRight: Radius.circular(20)),
+      backgroundColor: Colors.transparent,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
       ),
-      child: Container(
-        color: Color.fromARGB(50, 150, 122, 225),
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              padding: EdgeInsets.zero,
-              child: UserAccountsDrawerHeader(
-                margin: EdgeInsets.zero,
-                decoration:
-                    BoxDecoration(color: Color.fromARGB(255, 108, 99, 255)),
-                accountName: Text("User Name"),
-                accountEmail: Text("user@gmail.com"),
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: AssetImage("assets/images/dp.jpg"),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Color.fromARGB(255, 207, 204, 252),
+          ),
+          // margin: EdgeInsets.all(20),
+
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                padding: EdgeInsets.zero,
+                
+                child: UserAccountsDrawerHeader(
+                  margin: EdgeInsets.zero,
+                  
+                  decoration:
+                      BoxDecoration(color: Color.fromARGB(255, 168, 164, 250)),
+                      // Color.fromARGB(255, 108, 99, 255)
+                  accountName: Text("User Name"),
+                  accountEmail: Text("user@gmail.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/dp.jpg"),//user image
+                  ),
                 ),
               ),
-            ),
-            ListTile(
-              leading: Icon(
-                CupertinoIcons.home,
-                color: Color.fromARGB(255, 0, 3, 1),
+              ListTile(
+                leading: Icon(
+                  CupertinoIcons.home,
+                  color: Color.fromARGB(255, 0, 3, 1),
+                ),
+                title: Text(
+                  "Home",
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
-              title: Text(
-                "Home",
-                style: TextStyle(color: Colors.black),
+              ListTile(
+                leading: Icon(
+                  CupertinoIcons.profile_circled,
+                  color: Color.fromARGB(255, 0, 3, 1),
+                ),
+                title: Text(
+                  "Profile",
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
-            ),
-            ListTile(
-              leading: Icon(
-                CupertinoIcons.profile_circled,
-                color: Color.fromARGB(255, 0, 3, 1),
+              ListTile(
+                leading: Icon(
+                  CupertinoIcons.heart,
+                  color: Color.fromARGB(255, 0, 3, 1),
+                ),
+                title: Text(
+                  "Favorites",
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
-              title: Text(
-                "Profile",
-                style: TextStyle(color: Colors.black),
+              ListTile(
+                leading: Icon(
+                  CupertinoIcons.cart,
+                  color: Color.fromARGB(255, 0, 3, 1),
+                ),
+                title: Text(
+                  "Cart",
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
-            ),
-            ListTile(
-              leading: Icon(
-                CupertinoIcons.heart,
-                color: Color.fromARGB(255, 0, 3, 1),
+              ListTile(
+                leading: Icon(
+                  CupertinoIcons.arrow_down_right_arrow_up_left,
+                  color: Color.fromARGB(255, 0, 3, 1),
+                ),
+                title: Text(
+                  "Transactions",
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
-              title: Text(
-                "Favorites",
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            ListTile(
-              leading: Icon(
-                CupertinoIcons.cart,
-                color: Color.fromARGB(255, 0, 3, 1),
-              ),
-              title: Text(
-                "Cart",
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            ListTile(
-              leading: Icon(
-                CupertinoIcons.arrow_down_right_arrow_up_left,
-                color: Color.fromARGB(255, 0, 3, 1),
-              ),
-              title: Text(
-                "Transactions",
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            ListTile(
-              leading: Icon(
-                CupertinoIcons.pencil_ellipsis_rectangle,
-                color: Color.fromARGB(255, 0, 3, 1),
-              ),
-              title: Text(
-                "Edit details",
-                style: TextStyle(color: Colors.black),
-              ),
-            )
-          ],
+              ListTile(
+                leading: Icon(
+                  CupertinoIcons.pencil_ellipsis_rectangle,
+                  color: Color.fromARGB(255, 0, 3, 1),
+                ),
+                title: Text(
+                  "Edit details",
+                  style: TextStyle(color: Colors.black),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
